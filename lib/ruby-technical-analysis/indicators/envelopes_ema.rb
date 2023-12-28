b8d2501f2d9db581f10384a3d481e26ba5dc8379
Brad Saterfiel <brad.saterfiel@gmail.com>
2023-12-28 14:36:54 -0600

    Update EEMA with base class

--- lib/ruby-technical-analysis/indicators/envelopes_ema.rb
+++ lib/ruby-technical-analysis/indicators/envelopes_ema.rb
@@ -1,26 +1,41 @@
 # frozen_string_literal: true
 
+require_relative "indicator"
 require_relative "../../ruby-technical-analysis/moving_averages"
 
 module RTA
   # Envelopes EMA indicator
   # Returns an array of current high, middle and low eema values
-  class EnvelopesEma
-    attr_accessor :price_series, :period, :percent
+  class EnvelopesEma < Indicator
+    attr_reader :period, :percent
 
     def initialize(price_series, period, percent)
-      @price_series = price_series
       @period = period
       @percent = percent
+
+      super(price_series)
     end
 
     def call
-      eema = RTA::MovingAverages.new(price_series.last(period)).ema(period)
+      caluculate_envelopes_ema
+    end
 
-      eema_up = (eema * (100 + percent)) / 100
-      eema_down = (eema * (100 - percent)) / 100
+    private
 
-      [eema_up, eema, eema_down].map { |val| val.truncate(3) }
+    def _eema
+      @_eema ||= moving_averages(period).ema(period)
+    end
+
+    def eema_up
+      (_eema * (100 + percent)) / 100
+    end
+
+    def eema_down
+      (_eema * (100 - percent)) / 100
+    end
+
+    def caluculate_envelopes_ema
+      [eema_up, _eema, eema_down].map { |val| val.truncate(3) }
     end
   end
 end
--- lib/ruby-technical-analysis/indicators/indicator.rb
+++ lib/ruby-technical-analysis/indicators/indicator.rb
@@ -16,15 +16,25 @@ module RTA
     private
 
     def extract_highs_lows_closes_volumes(period = nil)
-      series = period ? @price_series.last(period) : @price_series
+      series = period ? price_series.last(period) : price_series
 
       highs, lows, closes, volumes = series.transpose
 
       [highs, lows, closes, volumes]
     end
 
-    def moving_averages
-      RTA::MovingAverages.new(price_series)
+    def extract_highs_lows_closes(period = nil)
+      series = period ? price_series.last(period) : price_series
+
+      highs, lows, closes = series.transpose
+
+      [highs, lows, closes]
+    end
+
+    def moving_averages(period = nil)
+      series = period ? price_series.last(period) : price_series
+
+      RTA::MovingAverages.new(series)
     end
 
     def statistical_methods
