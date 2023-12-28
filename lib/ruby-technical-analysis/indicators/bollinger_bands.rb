3dda1d97e7149deafbc7a3f24dc83f0ef7bbc575
Brad Saterfiel <brad.saterfiel@gmail.com>
2023-12-28 13:43:07 -0600

    Update Bollinger Bands with base class

--- lib/ruby-technical-analysis/indicators/bollinger_bands.rb
+++ lib/ruby-technical-analysis/indicators/bollinger_bands.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
+require_relative "indicator"
 require_relative "../../ruby-technical-analysis/moving_averages"
 require_relative "../../ruby-technical-analysis/statistical_methods"
 
 module RTA
   # Bollinger Bands indicator
   # Returns an array containing the current upper, middle, and lower bands of the series
-  class BollingerBands
-    attr_accessor :price_series, :period
+  class BollingerBands < Indicator
+    attr_reader :period
 
     def initialize(price_series, period = 5)
-      @price_series = price_series
       @period = period
+
+      super(price_series)
     end
 
     def call
