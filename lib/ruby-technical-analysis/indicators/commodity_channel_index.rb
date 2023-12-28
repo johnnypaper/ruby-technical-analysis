c984028103f4fed69077a0f59c0f70ba2d725739
Brad Saterfiel <brad.saterfiel@gmail.com>
2023-12-28 14:40:19 -0600

    Refactor CCI a bit more

--- lib/ruby-technical-analysis/indicators/commodity_channel_index.rb
+++ lib/ruby-technical-analysis/indicators/commodity_channel_index.rb
@@ -35,39 +35,28 @@ module RTA
       @_typical_prices ||= calculate_typical_prices
     end
 
-    def calculate_typical_prices_sma
-      _typical_prices.each_cons(period).map do |tp|
-        RTA::MovingAverages.new(tp).sma(period)
-      end
-    end
-
     def _typical_prices_sma
-      @_typical_prices_sma ||= calculate_typical_prices_sma
-    end
-
-    def calculate_period_sum
-      _typical_prices.last(period).sum { |tp| (_typical_prices_sma.last - tp).abs }
+      @_typical_prices_sma ||=
+        _typical_prices.each_cons(period).map do |tp|
+          RTA::MovingAverages.new(tp).sma(period)
+        end
     end
 
     def _period_sum
-      @_period_sum ||= calculate_period_sum
-    end
-
-    def calculate_period_sum_next_period
-      (_period_sum.to_f / period) * 0.015
+      @_period_sum ||=
+        _typical_prices.last(period).sum do |tp|
+          (_typical_prices_sma.last - tp).abs
+        end
     end
 
     def _period_sum_next_period
-      @_period_sum_next_period ||= calculate_period_sum_next_period
-    end
-
-    def calculate_typical_prices_sma_minus_typical_prices
-      _typical_prices.last(period).last - _typical_prices_sma.last
+      @_period_sum_next_period ||=
+        (_period_sum.to_f / period) * 0.015
     end
 
     def _typical_prices_sma_minus_typical_prices
       @_typical_prices_sma_minus_typical_prices ||=
-        calculate_typical_prices_sma_minus_typical_prices
+        _typical_prices.last(period).last - _typical_prices_sma.last
     end
 
     def calculate_cci
