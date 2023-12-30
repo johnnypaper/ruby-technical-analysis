f064de8de70955c1feba57e547a39f854e89b53b
Brad Saterfiel <brad.saterfiel@gmail.com>
2023-12-30 11:53:36 -0600

    Refactor Statistical Methods

--- lib/ruby_technical_analysis/statistical_methods.rb
+++ lib/ruby_technical_analysis/statistical_methods.rb
@@ -1,30 +1,24 @@
 module RubyTechnicalAnalysis
   # Statistical methods used in calculations
-  class StatisticalMethods
-    attr_accessor :price_series
-
-    def initialize(price_series)
-      @price_series = price_series
-    end
-
+  class StatisticalMethods < Indicator
     def mean
-      @price_series.reduce(:+) / @price_series.length.to_f
+      price_series.reduce(:+) / price_series.length.to_f
     end
 
     def standard_deviation
-      return 0 if @price_series.uniq.length == 1
+      return 0 if price_series.uniq.length == 1
 
       Math.sqrt(variance)
     end
 
     def variance
-      squared_differences.reduce(:+) / @price_series.length.to_f
+      squared_differences.reduce(:+) / price_series.length.to_f
     end
 
     private
 
     def squared_differences
-      @price_series.map { |n| (n - mean)**2 }
+      price_series.map { |value| (value - mean)**2 }
     end
   end
 end
